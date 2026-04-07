6902b57d749bb5d88da54e6c998a6e529457b77c
Sonu Singh <shadow.pentest1899@gmail.com>
2026-04-07 11:09:57 +0530

    three way to create arraylist

--- List/arraylist1.java
+++ List/arraylist1.java
@@ -1,9 +1,14 @@
 package List;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class arraylist1 {
     public static void main(String[] args) {
+
+        // ways to create arraylist
+        // 1
         ArrayList<Integer> list = new ArrayList<>();
         list.add(2);
         list.add(2);
@@ -12,5 +17,14 @@ public class arraylist1 {
         list.add(2);
         list.add(2);
         System.out.println(list);
+
+        // 2
+        ArrayList<String> list2 = new ArrayList<>(List.of("sonu","vishal","rahul"));
+        System.out.println(list2);
+
+
+        // 3
+        List<String> list3 = Arrays.asList("sonu","monu","monuuuuu");
+        System.out.println(list3);
     }
 }
