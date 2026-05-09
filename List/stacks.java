5acf63df5cbc9d76ce7f742ddbc5ff02bed4ed4a
Sonu Singh <shadow.pentest1899@gmail.com>
2026-05-09 10:12:34 +0530

    add a notes file and learn about stack

---
+++ List/stacks.java
@@ -0,0 +1,28 @@
+package List;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class stacks {
+    public static void main(String[] args) {
+        // this is how to impliment stack 
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        System.out.println(stack);
+        System.out.println(stack.search(2));
+
+        // we can use linkedlist as a stack form
+        LinkedList<Integer> nums = new LinkedList<Integer>();
+        nums.addLast(1);
+        nums.addLast(2);
+        nums.addLast(3);
+        nums.addLast(4);
+        nums.addLast(5);
+        System.out.println(nums);
+        System.out.println(nums.indexOf(4));
+    }
+}
